51c09eaf761c1a0dc4ca48a37b175a1bfe2905e5
Umar Saleem <umarsaleem512452@gmail.com>
2024-09-03 11:44:14 +0500

    add form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user');
 });
+Route::get('/create-user',[UserController::class , 'store']);
